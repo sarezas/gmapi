009b144f78c2534cf4f303e48e42e9b8e7680bb7
Sharis <sarezas@gmail.com>
2019-04-23 15:25:27 +0300

    geolocation implemented

--- src/app/components/geolocation/geolocation.component.ts
+++ src/app/components/geolocation/geolocation.component.ts
@@ -10,15 +10,51 @@ import {} from 'googlemaps';
 export class GeolocationComponent implements OnInit {
   @ViewChild('map') mapEl: ElementRef;
   map: google.maps.Map;
+  marker: google.maps.Marker;
+  cString = 'You';
+  infoWindow = new google.maps.InfoWindow;
   constructor() { }
 
   ngOnInit() {
+    // set map properties
     const mapProperties = {
-         center: new google.maps.LatLng(54.8985, 23.9036),
-         zoom: 15,
-         mapTypeId: google.maps.MapTypeId.ROADMAP
+      center: new google.maps.LatLng(54.8985, 23.9036),
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
     };
+
+    // init map
     this.map = new google.maps.Map(this.mapEl.nativeElement, mapProperties);
- }
+
+    // delay prompt for geolocation usage
+    setTimeout(() => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+          const clientPosition = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          this.infoWindow.setPosition(clientPosition);
+          this.infoWindow.setContent('Your position');
+          this.infoWindow.open(this.map);
+          this.map.setCenter(clientPosition);
+          this.marker = new google.maps.Marker({position: clientPosition, map: this.map, draggable: true});
+        }, () => {
+          handleLocationError(true, this.map, this.map.getCenter());
+        });
+      } else {
+        handleLocationError(false, this.map, this.map.getCenter());
+      }
+    }, 1500);
+
+    // handle geolocation errors
+    function handleLocationError(browserHasGeolocation: boolean, infoWindow: any, pos: any) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(this.map);
+    }
+  }
 
 }
